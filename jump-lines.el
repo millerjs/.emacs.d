e09d8b40da0b11d65c173ee6eb76e8057b3894d2
Joshua Miller <jshuasmiller@gmail.com>
2013-10-21 16:35:20 -0500

    quick commit, too lazy to comment

--- jump-lines.el
+++ jump-lines.el
@@ -9,6 +9,18 @@
 
 )
 
+(defun jump-forward (lines)
+  "Jump forward n lines"
+  (interactive "s")
+  (next-line (/ (search lines "     a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z aa bb cc dd ee ff gg hh ii jj kk ll mm nn oo pp qq rr ss tt uu vv ww xx yy zzaaabbbcccdddeeefffhhhgggiiijjjkkklllmmmnnnooopppqqqrrrssstttuuuvvvwwwxxxyyyzzz") 3)))
+
+
+(defun jump-backward (lines)
+  "Jump backward n lines"
+  (interactive "s")
+  (previous-line (/ (search lines "     a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z aa bb cc dd ee ff gg hh ii jj kk ll mm nn oo pp qq rr ss tt uu vv ww xx yy zzaaabbbcccdddeeefffhhhgggiiijjjkkklllmmmnnnooopppqqqrrrssstttuuuvvvwwwxxxyyyzzz") 3)))
+
+
 
 ;; (defun jump-relative (line)
 ;;   "Special goto-line for rel."
@@ -31,18 +43,4 @@
 ;;   (interactive "s")
 ;;   (next-line (* -1 (string-to-int lines)))
 ;; )
-
-
-(defun jump-forward (lines)
-  "Jump forward n lines"
-  (interactive "s")
-  (next-line (/ (search lines "     a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z aa bb cc dd ee ff gg hh ii jj kk ll mm nn oo pp qq rr ss tt uu vv ww xx yy zzaaabbbcccdddeeefffhhhgggiiijjjkkklllmmmnnnooopppqqqrrrssstttuuuvvvwwwxxxyyyzzz") 3)))
-
-
-(defun jump-backward (lines)
-  "Jump backward n lines"
-  (interactive "s")
-  (previous-line (/ (search lines "     a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z aa bb cc dd ee ff gg hh ii jj kk ll mm nn oo pp qq rr ss tt uu vv ww xx yy zzaaabbbcccdddeeefffhhhgggiiijjjkkklllmmmnnnooopppqqqrrrssstttuuuvvvwwwxxxyyyzzz") 3)))
-
-
 
