1be126940484fbf685a3f28aea9bfbfee0c4ce8f
Joshua Miller <jshuasmiller@gmail.com>
2014-10-01 15:51:18 +0000

    added yaml mode

--- init.el
+++ init.el
@@ -39,6 +39,12 @@
 (add-hook 'python-mode-hook (lambda () (hs-minor-mode 1)))
 
 
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
+(add-hook 'yaml-mode-hook
+          '(lambda () (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
 
 ;; ----------- [ Bindings
 
