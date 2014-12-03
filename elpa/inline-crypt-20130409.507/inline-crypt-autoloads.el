1d43db2f85c00d7c2c2bf8e6e843dc3874f92388
Joshua Miller <jshuasmiller@gmail.com>
2014-12-03 12:39:04 -0600

    inline-crypt

---
+++ elpa/inline-crypt-20130409.507/inline-crypt-autoloads.el
@@ -0,0 +1,18 @@
+;;; inline-crypt-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("inline-crypt-pkg.el" "inline-crypt.el")
+;;;;;;  (21631 19613 92401 0))
+
+;;;***
+
+(provide 'inline-crypt-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; inline-crypt-autoloads.el ends here
---
+++ elpa/inline-crypt-20130409.507/inline-crypt-pkg.el
@@ -0,0 +1,4 @@
+(define-package "inline-crypt" "20130409.507" "Simple inline encryption via openssl" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
