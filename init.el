224b1383ce435a3ef012d5b33a828229ad8a32a5
Joshua Miller <jsmiller@uchicago.edu>
2016-08-27 12:01:29 -0500

    with init--install-packages

--- init.el
+++ init.el
@@ -1,12 +1,17 @@
 (setq inhibit-startup-message t)
 
 ;; Set path to dependencies
-(setq site-lisp-dir (expand-file-name "custom-lisp" user-emacs-directory))
+(setq site-lisp-dir (expand-file-name "site-lisp" user-emacs-directory))
 (setq settings-dir (expand-file-name "settings" user-emacs-directory))
 
 ;; Set up load path
-(add-to-list 'load-path settings-dir)
 (add-to-list 'load-path site-lisp-dir)
+(add-to-list 'load-path settings-dir)
+
+;; Add external projects to load path
+(dolist (project (directory-files site-lisp-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
 
 (require 'misc-settings)
 
